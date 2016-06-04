312d0d77ec4b56045038d2663be26b79904daf76
nixan <nixan@NIXAN-32352646>
2016-06-04 15:28:08 +0430

    Add project files.

---
+++ getPing/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Windows.Forms;
+
+namespace getPing
+{
+    public partial class Form1 : Form
+    {
+        String[] ips = { "192.168.1.1", "192.168.1.61", "78.38.255.100 ", "10.10.53.209", "8.8.8.8", "4.2.2.4"  };
+        bool[] rtrn = new bool[6];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                rtrn[i]=PingHost(ips[i]);
+            }
+        }
+        public static bool PingHost(string nameOrAddress)
+        {
+            bool pingable = false;
+            Ping pinger = new Ping();
+            try
+            {
+                PingReply reply = pinger.Send(nameOrAddress);
+                pingable = reply.Status == IPStatus.Success;
+            }
+            catch (PingException)
+            {
+                // Discard PingExceptions and return false;
+            }
+            return pingable;
+        }
+    }
+}
